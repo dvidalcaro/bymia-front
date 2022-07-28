dbbcc9bf62c1db592eff9c2d1c70db2a1f7fe314
Diego Vidal Caro <dvidalcaro@hotmail.com>
2022-07-28 18:18:36 -0300

    primer commit

---
+++ src/data/header-navigation.ts
@@ -0,0 +1,9 @@
+import { NavigationLink } from '../app/shared/interfaces/navigation-link';
+
+export const navigation: NavigationLink[] = [
+    {label: 'Inicio', url: '/'},
+    {label: 'Ofertas', url: '/'},
+    {label: 'Historial', url: '/'},
+    {label: 'Moda', url: '/'},
+    {label: 'Ayuda', url: '/'},
+];
